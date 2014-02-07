9dccf2a4ee52170229c61cd724484ca455ed2ce0
Andrey Smirnov <Smirnov.Andrey@gmail.com>
2014-02-07 13:28:09 +0400

    System test for aptly snapshot create empty.

--- system/t05_snapshot/create.py
+++ system/t05_snapshot/create.py
@@ -34,3 +34,26 @@ class CreateSnapshot3Test(BaseTest):
     fixtureCmds = ["aptly snapshot create snap1 from mirror wheezy-main"]
     runCmd = "aptly snapshot create snap1 from mirror wheezy-contrib"
     expectedCode = 1
+
+
+class CreateSnapshot4Test(BaseTest):
+    """
+    create snapshot: empty
+    """
+    runCmd = "aptly snapshot create snap4 empty"
+
+    def check(self):
+        def remove_created_at(s):
+            return re.sub(r"Created At: [0-9:A-Za-z -]+\n", "", s)
+
+        self.check_output()
+        self.check_cmd_output("aptly snapshot show -with-packages snap4", "snapshot_show", match_prepare=remove_created_at)
+
+
+class CreateSnapshot5Test(BaseTest):
+    """
+    create snapshot: empty duplicate name
+    """
+    fixtureCmds = ["aptly snapshot create snap5 empty"]
+    runCmd = "aptly snapshot create snap5 empty"
+    expectedCode = 1
