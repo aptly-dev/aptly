b8fd33a92cd072fb354aae0e1beb4187278c94fb
Andrey Smirnov <Smirnov.Andrey@gmail.com>
2014-02-17 20:52:13 +0400

    Add wheezy snapshots with sources.

--- system/fixture.sh
+++ system/fixture.sh
@@ -10,6 +10,18 @@ aptly mirror update wheezy-non-free
 aptly mirror update wheezy-updates
 aptly mirror update wheezy-backports
 
+aptly -architectures=i386,amd64 mirror create -with-sources wheezy-main-src http://mirror.yandex.ru/debian/ wheezy main
+aptly -architectures=i386,amd64 mirror create -with-sources wheezy-contrib-src http://mirror.yandex.ru/debian/ wheezy contrib
+aptly -architectures=i386,amd64 mirror create -with-sources wheezy-non-free-src http://mirror.yandex.ru/debian/ wheezy non-free
+aptly -architectures=i386,amd64 mirror create -with-sources wheezy-updates-src http://mirror.yandex.ru/debian/ wheezy-updates
+aptly -architectures=i386,amd64 mirror create -with-sources wheezy-backports-src http://mirror.yandex.ru/debian/ wheezy-backports
+
+aptly mirror update wheezy-main-src
+aptly mirror update wheezy-contrib-src
+aptly mirror update wheezy-non-free-src
+aptly mirror update wheezy-updates-src
+aptly mirror update wheezy-backports-src
+
 aptly mirror create gnuplot-maverick http://ppa.launchpad.net/gladky-anton/gnuplot/ubuntu/ maverick
 aptly mirror update gnuplot-maverick
 
