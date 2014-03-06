81dd5a398b0ae7924fe54dfc0c7ca77e1753cdff
Andrey Smirnov <Smirnov.Andrey@gmail.com>
2014-03-06 23:46:06 +0400

    Uncomment lines.

--- system/t05_snapshot/__init__.py
+++ system/t05_snapshot/__init__.py
@@ -2,11 +2,11 @@
 Testing snapshot management
 """
 
-# from .create import *
-# from .show import *
-# from .list import *
-# from .verify import *
-# from .pull import *
+from .create import *
+from .show import *
+from .list import *
+from .verify import *
+from .pull import *
 from .diff import *
-# from .merge import *
-# from .drop import *
+from .merge import *
+from .drop import *
