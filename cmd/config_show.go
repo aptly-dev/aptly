8efb7903b2d4c6630d22458c827e8b5a1175a0e0
Simon Aquino <simon.aquino@digital.cabinet-office.gov.uk>
2014-08-06 00:19:55 +0000

    Added map to to_string and tabs to config_show

--- cmd/config_show.go
+++ cmd/config_show.go
@@ -12,12 +12,13 @@ func aptlyConfigShow(cmd *commander.Command, args []string) error {
 
 	config := context.Config()
 
-	fmt.Println(to_string(reflect.ValueOf(config).Elem()))
-	return err
+	fmt.Println(to_string(reflect.ValueOf(config).Elem(), 0))
+
+	return nil
 
 }
 
-func to_string(v reflect.Value) string {
+func to_string(v reflect.Value, tabs int) string {
 
 	switch v.Kind() {
 
@@ -28,24 +29,39 @@ func to_string(v reflect.Value) string {
 	case reflect.Slice:
 		var str_slice []string
 		for i := 0; i < v.Len(); i++ {
-			str_slice = append(str_slice, to_string(v.Index(i)))
+			str_slice = append(str_slice, to_string(v.Index(i), tabs))
 		}
 		return strings.Join(str_slice, ", ")
 	case reflect.Struct:
 		var str_slice []string
 		typ := reflect.TypeOf(v.Interface())
+		//str_slice = append(str_slice, make_tabs(tabs)+"{")
 		for i := 0; i < typ.NumField(); i++ {
-			str_slice = append(str_slice, typ.Field(i).Name+": "+to_string(v.Field(i)))
+			str_slice = append(str_slice, make_tabs(tabs)+typ.Field(i).Name+": "+to_string(v.Field(i), tabs+1))
 		}
+		//str_slice = append(str_slice, make_tabs(tabs)+"}")
 		return strings.Join(str_slice, "\n")
-	//case reflect.Map:
-	//	var str_slice []string
-
+	case reflect.Map:
+		var str_slice []string
+		str_slice = append(str_slice, "")
+		for _, key := range v.MapKeys() {
+			str_slice = append(str_slice, make_tabs(tabs)+"- "+to_string(key, tabs)+":\n"+to_string(v.MapIndex(key), tabs+1))
+		}
+		//str_slice = append(str_slice, make_tabs(tabs)+"}")
+		return strings.Join(str_slice, "\n")
 	case reflect.String:
 		return v.String()
 	}
 	return ""
 
+}
+
+func make_tabs(tabs int) string {
+	str := ""
+	for i := 0; i < tabs; i++ {
+		str += "\t"
+	}
+	return str
 }
 
 func makeCmdConfigShow() *commander.Command {
