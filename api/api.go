93e8e18ca6c76edfbd90c18038f4765372e0dbdd
Andrey Smirnov <Smirnov.Andrey@gmail.com>
2014-12-23 00:59:29 +0300

    Document lock order acquisition. #116 [ci skip]

--- api/api.go
+++ api/api.go
@@ -1,2 +1,8 @@
 // Package api provides implementation of aptly REST API
 package api
+
+// Lock order acquisition (canonical):
+//  1. RemoteRepoCollection
+//  2. LocalRepoCollection
+//  3. SnapshotCollection
+//  4. PublishedRepoCollection
