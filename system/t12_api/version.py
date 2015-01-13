c832a5cdc4d7ce4e71f97db04c164e44aa9ba211
Andrey Smirnov <Smirnov.Andrey@gmail.com>
2015-01-13 18:50:09 +0300

    Add missed file. #167

---
+++ system/t12_api/version.py
@@ -0,0 +1,10 @@
+from api_lib import APITest
+
+
+class VersionAPITest(APITest):
+    """
+    GET /version
+    """
+
+    def check(self):
+        self.check_equal(self.get("/api/version").json(), {'Version': '0.9~dev'})
