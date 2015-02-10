20adfd49a7a23bedccc9b9e93bf953921a1e6399
Sebastien Badia <sbadia@redhat.com>
2015-02-10 11:46:11 +0100

    swift: Add support for Swift API v1 (without Keystone)
    This commit also add a workaround for bug/feature[1]
    the password is changed every time :-)
    
    [1]https://github.com/ccollicutt/docker-swift-onlyone/commit/c9f5e41b745eee18e7ddc807481bc9729d8cdac0

--- .swift.sh
+++ .swift.sh
@@ -1,6 +1,6 @@
-export OS_AUTH_URL=http://127.0.0.1:8181/v2.0/
-export OS_USERNAME=user_test
-export OS_PASSWORD=tester
-export OS_TENANT_NAME=testing
+export ST_AUTH=http://127.0.0.1:8181/auth/v1.0
+export ST_USER=test:tester
+ID=`docker run -d -p 8080:8080 serverascode/swift-onlyone`
+sleep 10 # Give the script that change the passwords some time
+export ST_KEY=`docker logs $ID | grep "user_test_tester =" | cut -d " " -f 3`
 pip install python-keystoneclient python-swiftclient
-docker run -d -p 8080:8080 serverascode/swift-onlyone
