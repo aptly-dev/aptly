747693772c01d4e32b366e28cdafd0a23ad2dcbf
Oliver Sauder <oliver.sauder@adfinis-sygroup.ch>
2016-11-25 16:25:09 +0100

    Solving progress not safe issue for api
    Progress is not safe so for api its always nil and
    code needs to take care of this

--- http/download.go
+++ http/download.go
@@ -152,6 +152,7 @@ func retryableError(err error) bool {
 
 func (downloader *downloaderImpl) newRequest(ctx context.Context, method, url string) (*http.Request, error) {
 	req, err := http.NewRequest(method, url, nil)
+
 	if err != nil {
 		return nil, errors.Wrap(err, url)
 	}
@@ -230,7 +231,11 @@ func (downloader *downloaderImpl) download(req *http.Request, url, destination s
 	defer outfile.Close()
 
 	checksummer := utils.NewChecksumWriter()
-	writers := []io.Writer{outfile, downloader.aggWriter}
+	writers := []io.Writer{outfile}
+
+	if downloader.progress != nil {
+		writers = append(writers, downloader.progress)
+	}
 
 	if expected != nil {
 		writers = append(writers, checksummer)
