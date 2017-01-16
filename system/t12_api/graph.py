96948d6f18fe9ecc6c59424acffdb78765b83ac5
jolo <johannes.layher@googlemail.com>
2017-01-16 23:31:45 +0100

    Basic test of graph layout

--- system/t12_api/graph.py
+++ system/t12_api/graph.py
@@ -1,4 +1,5 @@
 from api_lib import APITest
+import xml.etree.ElementTree as ET
 
 
 class GraphAPITest(APITest):
@@ -15,3 +16,20 @@ class GraphAPITest(APITest):
         resp = self.get("/api/graph.svg")
         self.check_equal(resp.headers["Content-Type"], "image/svg+xml")
         self.check_equal(resp.content[:4], '<?xm')
+
+        # make sure our default layout is horizontal
+        default = self.get("/api/graph.svg").content
+        horizontal = self.get("/api/graph.svg?layout=horizontal").content
+        self.check_equal(default, horizontal)
+
+        # basic test of layout:
+        # horizontal should be wider than vertical
+        # vertical should be higher than horizontal
+        vertical = self.get("/api/graph.svg?layout=vertical").content
+        svgHWidth = ET.fromstring(horizontal).get('width').replace("pt","")
+        svgHHeight = ET.fromstring(horizontal).get('height').replace("pt","")
+        svgVWidth = ET.fromstring(vertical).get('width').replace("pt","")
+        svgVHeight = ET.fromstring(vertical).get('height').replace("pt","")
+
+        self.check_gt(svgHWidth, svgVWidth)
+        self.check_gt(svgVHeight, svgHHeight)
