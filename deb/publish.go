80e350905f7bc68d8c5bc9226bd1276813f35135
Oliver Sauder <oliver.sauder@adfinis-sygroup.ch>
2018-05-17 15:43:16 +0200

    Combine publish list progress into one

--- deb/publish.go
+++ deb/publish.go
@@ -562,6 +562,14 @@ func (p *PublishedRepo) Publish(packagePool aptly.PackagePool, publishedStorageP
 	indexes := newIndexFiles(publishedStorage, basePath, tempDir, suffix, p.AcquireByHash)
 
 	legacyContentIndexes := map[string]*ContentsIndex{}
+	var count int64
+	for _, list := range lists {
+		count = count + int64(list.Len())
+	}
+
+	if progress != nil {
+		progress.InitBar(count, false)
+	}
 
 	for component, list := range lists {
 		hadUdebs := false
@@ -569,10 +577,6 @@ func (p *PublishedRepo) Publish(packagePool aptly.PackagePool, publishedStorageP
 		// For all architectures, pregenerate packages/sources files
 		for _, arch := range p.Architectures {
 			indexes.PackageIndex(component, arch, false)
-		}
-
-		if progress != nil {
-			progress.InitBar(int64(list.Len()), false)
 		}
 
 		list.PrepareIndex()
@@ -675,10 +679,6 @@ func (p *PublishedRepo) Publish(packagePool aptly.PackagePool, publishedStorageP
 			}
 		}
 
-		if progress != nil {
-			progress.ShutdownBar()
-		}
-
 		udebs := []bool{false}
 		if hadUdebs {
 			udebs = append(udebs, true)
@@ -737,6 +737,7 @@ func (p *PublishedRepo) Publish(packagePool aptly.PackagePool, publishedStorageP
 	}
 
 	if progress != nil {
+		progress.ShutdownBar()
 		progress.Printf("Finalizing metadata files...\n")
 	}
 
