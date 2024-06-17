9349c53ac663aa8a526f6f90e8fdcb13115b97fe
André Roth <neolynx@gmail.com>
2024-06-17 13:50:49 +0200

    fix unit tests

--- deb/snapshot.go
+++ deb/snapshot.go
@@ -45,7 +45,7 @@ type Snapshot struct {
 
 // NewSnapshotFromRepository creates snapshot from current state of repository
 func NewSnapshotFromRepository(name string, repo *RemoteRepo) (*Snapshot, error) {
-	if repo.packageRefs == nil {
+	if repo.packageRefs == nil || repo.packageRefs.Len() == 0 {
 		return nil, errors.New("mirror not updated")
 	}
 
