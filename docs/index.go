b1517ad0f652f8278e46271fa1f58c1062f5d55a
André Roth <neolynx@gmail.com>
2024-10-05 15:26:16 +0200

    docs: improve swagger
    - use markdown files in swagger
    - automate version, use swager.conf template
    - embed swagger ui index.html as docs.html

--- docs/index.go
+++ docs/index.go
@@ -1,3 +1,9 @@
 package docs
 
-import _ "github.com/swaggo/swag" // make sure swag is in go.mod
+import (
+	_ "embed"
+        _ "github.com/swaggo/swag" // make sure swag is in go.mod
+)
+
+//go:embed docs.html
+var DocsHTML []byte
